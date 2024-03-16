d95e065ce48655306d6dfc726cf7a6de0e796930
Oleg Kupavtsev <olegkupavtsev@gmail.com>
2024-03-16 19:02:41 +0300

    mp_test_done

--- tests/test.py
+++ tests/test.py
@@ -1,38 +1,65 @@
 
 # import array
+import numpy as np
 
 # a = array.array('i', [0]) * 2*9
 
-hl_list = [[4,1],[7,3],[8,6],[5,2]]
 
 # 1 2H
 # h, l = 4, 1
-period_ticks = [1,2,3,4,5,6,7,8]
-periods_mp = [0]*len(period_ticks)
-
+period_ticks = [ 1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.8 ]
+periods_mp   = [0]*len(period_ticks)
+example      = [0,0,0,0,0,0,0,0]
 def mp_calc(hl):
     h = hl[0]
     l = hl[1]
-    
-    example      = [0,0,0,0,0,0,0,0]
-    tick = 1
-
+    tick = 1.1
     for k in range(len(periods_mp)):
-        # print(period_ticks[k])
-        if period_ticks[k] in range(l, h+tick):
-            # print(periods_mp[k])
-            x = periods_mp[k]+1
-            periods_mp[k] = x
-            # print(periods_mp[k]+1)
-            # periods_mp = [i+1 for i in periods_mp[k]+1]
-
+        # if l >= period_ticks[k] and period_ticks[k] <= h:
+        if period_ticks[k] >= l and period_ticks[k] <= h:
+            # print(period_ticks[k])
+            periods_mp[k] = periods_mp[k]+1
+        # elif l >= period_ticks[k] and period_ticks[k] <= h:
+            # print(period_ticks[k])
+            # periods_mp[k] = periods_mp[k]+1
     print(periods_mp)
-
 def data_2h():
-    hl_list = [[4,1],[7,3],[8,6],[5,2]]
+    # hl_list = [[4,1], [7,3], [8,6], [5,2]]
+    hl_list = [[5.4, 2.2]]
     for hl in hl_list:
         mp_calc(hl)
-
-
 data_2h()
-print(periods_mp)
+# print(periods_mp)
+
+
+
+# Working version with Integers
+# period_ticks = [1.1,2.2,3,4,5,6,7,8.1]
+# periods_mp = [0]*len(period_ticks)
+
+# def mp_calc(hl):
+#     h = hl[0]
+#     l = hl[1]
+    
+#     example      = [0,0,0,0,0,0,0,0]
+#     tick = 1
+
+#     for k in range(len(periods_mp)):
+#         # print(period_ticks[k])
+#         if period_ticks[k] in range(l, h+tick):
+#             # print(periods_mp[k])
+#             x = periods_mp[k]+1
+#             periods_mp[k] = x
+#             # print(periods_mp[k]+1)
+#             # periods_mp = [i+1 for i in periods_mp[k]+1]
+
+#     print(periods_mp)
+
+# def data_2h():
+#     hl_list = [[4,1],[7,3],[8,6],[5,2]]
+#     for hl in hl_list:
+#         mp_calc(hl)
+
+
+# data_2h()
+# print(periods_mp)
